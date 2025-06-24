6484e4dcd1bbbcbb7b9ac0cb8ed1620e0d45ec07
dabbeshu <durveshadulkar@gmail.com>
2025-06-24 15:19:12 +0530

    Update UI of employee and join backend

--- lib/admin/auth_service.dart
+++ lib/admin/auth_service.dart
@@ -62,6 +62,71 @@
 //   }
 // }
 
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:quick_roll/services/global.dart';
+// import 'package:quick_roll/services/global_API.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// class AuthService {
+//   static Future<bool> isUserRegistered(
+//       String identifier, String password) async {
+//     final url = Uri.parse('$baseURL/login');
+
+//     final response = await http.post(
+//       url,
+//       headers: {"Content-Type": "application/json"},
+//       body: jsonEncode({"identifier": identifier, "password": password}),
+//     );
+
+//     if (response.statusCode == 200) {
+//       final Map<String, dynamic> responseBody = json.decode(response.body);
+//       print('Response: $responseBody');
+
+//       if (responseBody['status'] == 'success') {
+//         final admin = responseBody['data'];
+
+//         SharedPreferences prefs = await SharedPreferences.getInstance();
+//         await prefs.setString('loggedInUserId', admin['id'].toString());
+//         await prefs.setString('loggedInUserEmail', admin['email_id'] ?? '');
+//         await prefs.setString('loggedInUserContact', admin['user_name'] ?? '');
+//         await prefs.setString('loggedInUserCompanyId',
+//             admin['id'].toString()); // Store company_id
+//         await prefs.setString(
+//             'companyData', jsonEncode(responseBody)); // Save entire response
+
+//         global_cid = admin['id'];
+//         print('company id= $global_cid');
+//         print('Admin global_cid: $global_cid');
+//         print('User logged in successfully: $admin');
+//         return true;
+//       }
+//     } else {
+//       print('Login failed: ${response.statusCode}, Response: ${response.body}');
+//     }
+//     return false;
+//   }
+
+//   static Future<String?> getLoggedInUserEmail() async {
+//     SharedPreferences prefs = await SharedPreferences.getInstance();
+//     return prefs.getString('loggedInUserEmail');
+//   }
+
+//   static Future<String?> getLoggedInUserContact() async {
+//     SharedPreferences prefs = await SharedPreferences.getInstance();
+//     return prefs.getString('loggedInUserContact');
+//   }
+
+//   static Future<String?> getLoggedInUserId() async {
+//     SharedPreferences prefs = await SharedPreferences.getInstance();
+//     return prefs.getString('loggedInUserId');
+//   }
+
+//   static Future<String?> getLoggedInUserCompanyId() async {
+//     SharedPreferences prefs = await SharedPreferences.getInstance();
+//     return prefs.getString('loggedInUserCompanyId');
+//   }
+// }
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:quick_roll/services/global.dart';
@@ -90,10 +155,8 @@ class AuthService {
         await prefs.setString('loggedInUserId', admin['id'].toString());
         await prefs.setString('loggedInUserEmail', admin['email_id'] ?? '');
         await prefs.setString('loggedInUserContact', admin['user_name'] ?? '');
-        await prefs.setString('loggedInUserCompanyId',
-            admin['id'].toString()); // Store company_id
-        await prefs.setString(
-            'companyData', jsonEncode(responseBody)); // Save entire response
+        await prefs.setString('loggedInUserCompanyId', admin['id'].toString());
+        await prefs.setString('companyData', jsonEncode(responseBody));
 
         global_cid = admin['id'];
         print('company id= $global_cid');
@@ -105,6 +168,17 @@ class AuthService {
       print('Login failed: ${response.statusCode}, Response: ${response.body}');
     }
     return false;
+  }
+
+  static Future<void> logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('loggedInUserId');
+    await prefs.remove('loggedInUserEmail');
+    await prefs.remove('loggedInUserContact');
+    await prefs.remove('loggedInUserCompanyId');
+    await prefs.remove('companyData');
+    global_cid = 0;
+    print('User logged out, SharedPreferences cleared');
   }
 
   static Future<String?> getLoggedInUserEmail() async {
--- lib/model/signup_form_model.dart
+++ lib/model/signup_form_model.dart
@@ -633,6 +633,7 @@
 //     }
 //   }
 // }
+
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
